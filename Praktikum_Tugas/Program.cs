8aa6cff9b4ab67028ad5dc2fb8f57adc363e141f
Raffy <Raffy@LAPTOP-NDGD79A6>
2024-09-23 18:16:04 +0700

    Add project files.

---
+++ Praktikum_Tugas/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Singa singa1 = new Singa("Jawa", 6, 4);
+        Console.WriteLine(singa1.Suara());
+        singa1.Mengaum();
+    }
+}
+
+class Hewan
+{
+    public string nama;
+    public int umur;
+
+    public Hewan(string nama, int umur)
+    {
+        this.nama = nama;
+        this.umur = umur;
+    }
+
+    public virtual string Suara()
+    {
+        return "Hewan ini bersuara";
+    }
+
+    public string InfoHewan()
+    {
+        return $"Nama: {this.nama}\nUmur: {this.umur}";
+    }
+}
+
+class Mamalia : Hewan
+{
+    public int jumlahKaki;
+
+    public Mamalia(string nama, int umur, int jumlahKaki) : base (nama, umur)
+    {
+        this.jumlahKaki = jumlahKaki;
+    }
+
+    public override string Suara()
+    {
+        return "Mamalia ini bersuara";
+    }
+}
+
+class Singa : Mamalia
+{
+    public Singa (string nama, int umur, int jumlahKaki) : base (nama, umur, jumlahKaki)
+    {
+
+    }
+
+    public override string Suara()
+    {
+        return "rawr";
+    }
+
+    public void Mengaum()
+    {
+        Console.WriteLine($"Singa {this.nama} mengaum RAWR");
+    }
+}
