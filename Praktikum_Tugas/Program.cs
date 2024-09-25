346baa0758d172e1fedaeaecad816cdbaeee0cf4
Raffy Nugraha <raffy.nugraha12@outlook.com>
2024-09-25 20:20:13 +0700

    update##

--- Praktikum_Tugas/Program.cs
+++ Praktikum_Tugas/Program.cs
@@ -1,10 +1,42 @@
 ﻿class Program
 {
+    //TESTING
+
+    //HUHUHUHUHUHUHUU
     static void Main(string[] args)
     {
-        Singa singa1 = new Singa("Jawa", 6, 4);
-        Console.WriteLine(singa1.Suara());
-        singa1.Mengaum();
+        KebunBinatang zoo = new KebunBinatang();
+        Singa singa = new Singa("Singa", 5, 4);
+        Gajah gajah = new Gajah("Gajah", 8, 4);
+        Ular ular = new Ular("Ular", 3, 2.12);
+        Buaya buaya = new Buaya("Buaya", 12, 3.6);
+        zoo.TambahHewan(singa);
+        zoo.TambahHewan(gajah);
+        zoo.TambahHewan(ular);
+        zoo.TambahHewan(buaya);      
+        zoo.DaftarHewan();
+        Console.WriteLine(singa.Suara());
+        Console.WriteLine(gajah.Suara());
+        Console.WriteLine(ular.Suara());
+        Console.WriteLine(buaya.Suara());
+        singa.Mengaum();
+
+        //soal 1
+        Console.WriteLine(gajah.Suara());
+        Console.WriteLine(ular.Suara());
+
+        //soal 2
+        singa.Mengaum();
+
+        //soal 3
+        Console.WriteLine(singa.InfoHewan());
+
+        //soal 4
+        ular.Merayap();
+
+        //soal 5
+        Reptil reptil = new Buaya("Reptil", 3, 3.1);
+        Console.WriteLine(reptil.Suara());
     }
 }
 
@@ -24,9 +56,29 @@ class Hewan
         return "Hewan ini bersuara";
     }
 
-    public string InfoHewan()
+    public virtual string InfoHewan()
     {
-        return $"Nama: {this.nama}\nUmur: {this.umur}";
+        return $"Nama: {this.nama}\nUmur: {this.umur} tahun";
+    }
+}
+
+class KebunBinatang
+{
+    public List<Hewan> koleksiHewan = new List<Hewan>(); 
+
+    public void TambahHewan(Hewan hewan)
+    {
+        koleksiHewan.Add(hewan);
+        Console.WriteLine("Hewan berhasil ditambahkan");
+    }
+
+    public void DaftarHewan()
+    {
+        Console.WriteLine("Daftar hewan di kebun binatang:");
+        foreach (Hewan hewan in koleksiHewan)
+        {
+            Console.WriteLine(hewan.ToString());
+        }
     }
 }
 
@@ -34,7 +86,7 @@ class Mamalia : Hewan
 {
     public int jumlahKaki;
 
-    public Mamalia(string nama, int umur, int jumlahKaki) : base (nama, umur)
+    public Mamalia(string nama, int umur, int jumlahKaki) : base(nama, umur)
     {
         this.jumlahKaki = jumlahKaki;
     }
@@ -45,20 +97,79 @@ class Mamalia : Hewan
     }
 }
 
+class Reptil : Hewan
+{
+    public double panjangTubuh;
+
+    public Reptil(string nama, int umur, double panjangTubuh) : base(nama, umur)
+    {
+        this.panjangTubuh = panjangTubuh;
+    }
+}
+
 class Singa : Mamalia
 {
-    public Singa (string nama, int umur, int jumlahKaki) : base (nama, umur, jumlahKaki)
+    public Singa (string nama, int umur, int jumlahKaki) : base(nama, umur, jumlahKaki)
     {
 
     }
 
     public override string Suara()
     {
-        return "rawr";
+        return "Singa bersuara rawr";
+    }
+
+    public override string InfoHewan()
+    {
+        return $"{base.InfoHewan()}\nJumlah kaki: {this.jumlahKaki}";
     }
 
     public void Mengaum()
     {
-        Console.WriteLine($"Singa {this.nama} mengaum RAWR");
+        Console.WriteLine("Singa mengaum RAWR");
+    }
+}
+
+class Gajah : Mamalia
+{
+    public Gajah(string nama, int umur, int jumlahKaki) : base(nama, umur, jumlahKaki)
+    {
+
+    }
+
+    public override string Suara()
+    {
+        return "Gajah bersuara trotttt";
+    }
+}
+
+class Ular : Reptil
+{
+    public Ular(string nama, int umur, double panjangTubuh) : base(nama, umur, panjangTubuh)
+    {
+
+    }
+
+    public override string Suara()
+    {
+        return "Ular bersuara psssshh";
+    }
+
+    public void Merayap()
+    {
+        Console.WriteLine("Ular merayap di tanah");
+    }
+}
+
+class Buaya : Reptil
+{
+    public Buaya(string nama, int umur, double panjangTubuh) : base(nama, umur, panjangTubuh)
+    {
+
+    }
+
+    public override string Suara()
+    {
+        return "Buaya bersuara grrrrr";
     }
 }
